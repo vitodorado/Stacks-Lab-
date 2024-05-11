b735111eb27bb07f2b5f47e0cdca0be37d473f54
vitodorado <138409653+vitodorado@users.noreply.github.com>
2024-05-11 16:45:56 -0700

    Update InsertAtCommand.py
    changed the whole object to be what i have turned in

--- InsertAtCommand.py
+++ InsertAtCommand.py
@@ -7,21 +7,12 @@ class InsertAtCommand(UndoCommand):
         self.source_list = source
         self.index = index
         self.new_item = new_item
-        self.removed_item = None
         
 
     def execute(self):
-        
-        #remove the item at the specified index and store it 
-        self.removed_item = self.source_list.pop(self.index)
-        #Insert the new item at the specified index
-        self.source_list.insert(self.index, self.new_item)   
+        #insert the new item at the specified index 
+        self.source_list.insert(self.index, self.new_item)
     def undo(self):
-        print("Before undo - List:", self.source_list)
-        print("Before undo - Removed item:", self.removed_item)
-        #remove the new item that was inserted during the execute method
-        self.source_list.pop(self.index)
-        #reinsert the previously removed item at the original index
-        self.source_list.insert(self.index, self.removed_item)
-        print("After undo - List:", self.source_list)
+        del self.source_list[self.index]    
+      
       
